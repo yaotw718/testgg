45238240b3a0da2e07699c8184a2a4257182baae
xuyao <tim.xu@jwis.cn>
2016-11-17 11:22:08 +0800

    remove myprcess2222

--- MyProcess/lib/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by afterloe on 8/2/2016.
- */
-const path = require("path");
-
-module.exports = {
-    windowHelper: require(path.join(__dirname, "windowBuilder")),
-    netRequest: require(path.join(__dirname, "jobSystem"))
-};
